5ce78288a77c3643856d0b769d430e50814b1e8a
manoj05-smk <manoj.smk00122@gmail.com>
2026-03-09 20:17:19 +0530

    Basic C Programming Exercises

---
+++ Basics/Size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // Usage of sizeof() to know size the data types
+    printf("The size of int: %d\n", sizeof(int));
+    printf("The size of char: %d\n", sizeof(char));
+    printf("The size of float: %d\n", sizeof(float));
+    printf("The size of double: %d", sizeof(double));
+
+    return 0;
+}
