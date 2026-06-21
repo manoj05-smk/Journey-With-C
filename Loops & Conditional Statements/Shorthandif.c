1029aea64d66e004d48a24f10b32f7b3ab6b4ee0
manoj05-smk <manoj.smk00122@gmail.com>
2026-06-21 13:01:10 +0530

    Works on Ternary Operator

---
+++ Loops & Conditional Statements/Shorthandif.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    //Conditional Statement Using Ternary Operator
+    
+    int x=5,y=6;
+    int max = (x>y) ? x : y;
+    printf("Maximum is %d",max);
+    return 0;
+}
